714508e9d0cee48357a62b33d910b20fadb5876e
QureshiCodes16 <152991545+QureshiCodes69@users.noreply.github.com>
2024-05-16 01:37:59 +0500

    email_backend

--- server.js
+++ server.js
@@ -168,8 +168,6 @@ const Product = mongoose.model('Product', new mongoose.Schema({
 
 
 
-  
-
   const PORT = process.env.PORT || 8080;
   app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
